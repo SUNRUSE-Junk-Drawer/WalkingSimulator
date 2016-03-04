ba50ae9a92de3afe5313df9d4323c5f482325b40
James Wild <James Wild>
2016-03-04 21:50:03 +0000

    Improved fillrate on some low-end devices by baking the pow() into the noise texture used by the cloud renderer.

--- src/rendering/cloud.fs
+++ src/rendering/cloud.fs
@@ -7,6 +7,6 @@ varying vec3 var_color;
 uniform sampler2D brushstrokes;
 
 void main() {
-    if(pow(texture2D(brushstrokes, var_uv).r, 0.5) < length(var_uv)) discard;
+    if(texture2D(brushstrokes, var_uv).r < length(var_uv)) discard;
 	gl_FragColor = vec4(var_color, 1.0);
 }
